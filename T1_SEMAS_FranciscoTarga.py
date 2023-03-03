56511da5c783dad0eca7150eb572cfe70f2c7d99
Francisco Targa <francisco.targa@edu.pucrs.br>
2023-03-02 21:25:51 -0300

    Adiciomado gerador

---
+++ T1_SEMAS_FranciscoTarga.py
@@ -0,0 +1,16 @@
+def geradorNumsAleatorios(seed, a, c, m, num_aleatorios):
+    x = seed
+    numeros_aleatorios = []
+    for i in range(num_aleatorios):
+        x = (a*x + c) % m
+        num_aleatorio = x / m
+        numeros_aleatorios.append(num_aleatorio)
+    return numeros_aleatorios
+
+X0 = 666666
+a = 12324
+c = 342423
+M = 54354
+
+numeros_aleatorios = geradorNumsAleatorios(X0, a, c, M, 1000)
+print(numeros_aleatorios)
